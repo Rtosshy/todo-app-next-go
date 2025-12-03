b381f058ba7284847b3ad46aaa21a18417296d33
Rtosshy <tosshy820@gmail.com>
2025-12-03 21:53:03 +0900

    feat: hide password input

---
+++ frontend/src/app/ui/password-input.tsx
@@ -0,0 +1,33 @@
+'use client'
+
+import { useState } from 'react'
+import { Eye, EyeOff } from 'lucide-react'
+
+type PasswordInputProps = {
+  value: string
+  onChange: (value: string) => void
+  placeholder: string
+}
+
+export default function PasswordInput({ value, onChange, placeholder }: PasswordInputProps) {
+  const [showPassword, setShowPassword] = useState(false)
+
+  return (
+    <div className="relative w-85">
+      <input
+        type={showPassword ? "text" : "password"}
+        placeholder={placeholder}
+        className="mt-4 px-4 py-2 pr-12 border border-gray-300 rounded-md bg-white w-full text-black"
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+      />
+      <button
+        type="button"
+        onClick={() => setShowPassword(!showPassword)}
+        className="absolute right-3 top-1/2 mt-2 -translate-y-1/2 text-gray-500 hover:text-gray-700"
+      >
+        {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
+      </button>
+    </div>
+  )
+}
